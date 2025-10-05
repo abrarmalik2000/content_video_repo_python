e2efacd60a07ec10760eeaa30c2afd0c1790c049
Abrar Malik <a02478529@aggies.usu.edu>
2025-10-05 20:47:00 +0000

    week 7 video lectures practice(numpy)

---
+++ week7/contentvideos/week7_vid_lecture.py
@@ -0,0 +1,63 @@
+# # opening a file in read mode 
+# # file = open("D:\Courses USU\Intro to Python\example.txt", "r")
+
+# #reading the entire contents of the file 
+# # content = file.read()
+# # print("content :",content)
+# # print()
+
+# # read the file line by line 
+# # lines = file.readlines()
+# # for line in lines:
+# #     print("line: ", line)
+
+# # #closing the line 
+# # file.close()
+
+# #opening a file in write mode
+# file = open("output.txt", "w")
+
+# #writing data to the file 
+# file.write("Hello world !\n")
+# file.write("This is a new line in my file !!!\n")
+# # close the file 
+# # file.close()
+# content = file.read()
+# print(content)
+
+# import os
+# file1 = open("output1.txt", "w")
+# file1.write("hello\n")
+# file1.write("goodbye\n")
+# file1.close()
+
+# os.rename("output1.txt", "output_renamed.txt")
+# os.mkdir("fun_directory")
+# os.system("mkdir -p fun_directory/new_directory/new")
+# os.system ("ls")
+# os.system ("pwd")
+
+# os.mkdir("test_directory")
+
+# import numpy
+# create list of 4 floats 
+# homework_scores = [49.5, 48.3, 47.6, 50]
+# print(homework_scores)
+
+#convert the list to numpy array 
+# hw_np = numpy.array(homework_scores)
+# print (hw_np)
+ 
+# list = [0,1,2,3,4,5,6,76,8,9,10]
+# print (list)
+
+# list_np = numpy.array(list)
+# print(list_np)
+
+#initialize numpy array of size 10 
+import numpy
+np1 = numpy.zeros(100)
+print(np1)
+
+np2 = numpy.ones(100)
+print(np2)
