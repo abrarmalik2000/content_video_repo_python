d15f15085ec6b566ec7ffae1d51505e7cfd350a3
Abrar Malik <a02478529@aggies.usu.edu>
2025-11-16 20:41:30 +0000

    week13_vid_lec_code

--- week13/contentvideos/week13_vid_lectures_code.py
+++ week13/contentvideos/week13_vid_lectures_code.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import numpy
 
 url =  'https://api.datamuse.com/words?ml=duck&sp=b*&max=3'
 
@@ -9,6 +10,14 @@ request = requests.get(url)
 #print(request.text)
 
 rqst_dict = json.loads(request.text)
-print(rqst_dict)
+#print(rqst_dict)
 
 json.dump(rqst_dict, open("/workspaces/content_video_repo_python/week13/test.json", "w"))
+
+#print(rqst_dict.keys())
+
+scores = []
+for d in rqst_dict:
+    scores.append(d["score"])
+print("scores", scores)
+print ("avg score: ", numpy.mean(scores))
